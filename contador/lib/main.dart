fc62f50f61e1a2c71eaa000b0b235398e15e7547
Sebas <seplarui@gmail.com>
2020-07-11 11:48:58 +0200

    First commit

---
+++ contador/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+//Nuestras importaciones
+import 'src/app.dart';
+
+void main() {
+  runApp(MyApp()); //Espera un widget
+}
---
+++ contador/lib/src/pages/contador_page.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+
+class ContadorPage extends StatefulWidget {
+  @override
+  createState() {
+    return _ContadorPageState();
+  }
+}
+
+class _ContadorPageState extends State<ContadorPage> {
+  //clase privada por "_"
+
+  final _estiloTexto = new TextStyle(fontSize: 35);
+  int _conteo = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('StateFul'),
+        centerTitle: true,
+      ),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Número de clicks', style: _estiloTexto),
+          Text('$_conteo', style: _estiloTexto),
+        ],
+      )),
+      floatingActionButton: _crearBotones(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
+  }
+
+  Widget _crearBotones() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        SizedBox(width: 30),
+        FloatingActionButton(
+          child: Icon(Icons.exposure_zero),
+          onPressed: _reset,
+        ),
+        Expanded(child: SizedBox()),
+        FloatingActionButton(
+          child: Icon(Icons.remove),
+          onPressed: _sustraer,
+        ),
+        SizedBox(width: 5.0),
+        FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: _agregar,
+        ),
+      ],
+    );
+  }
+
+  void _agregar() {
+    setState(() => _conteo++);
+  }
+
+  void _sustraer() {
+    setState(() => _conteo--);
+  }
+
+  void _reset() {
+    setState(() => _conteo = 0);
+  }
+}
