0be89da0af8c999031766a084ddf081b6d2c26a5
Sebas <seplarui@gmail.com>
2020-07-11 17:35:18 +0200

    menu_provider.dart

--- componentes/lib/src/pages/home_temp.dart
+++ componentes/lib/src/pages/home_temp.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
  
 class HomePageTemp extends StatelessWidget {
+
+  final opciones = ['Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco'];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,13 +11,43 @@ class HomePageTemp extends StatelessWidget {
         title: Text ('Componentes Temp'),
       ),
       body: ListView(
-        children: <Widget>[
-          ListTile(
-            title: Text('ListTile Title'),
-          ),
-          Divider(),
-        ],
+        // children: _crearItems()
+        children: _crearItemsCorta()
       ),
     );
   }
+
+  List<Widget> _crearItems() {
+
+    List <Widget> lista = new List<Widget>();
+
+    for (String opt in opciones) {
+      final tempWidget = ListTile(
+        title: Text(opt),
+      );
+      lista.add(tempWidget);
+      lista.add(Divider(color:Colors.black));
+    }
+    return lista;
+  }
+
+  List<Widget> _crearItemsCorta() {
+    return opciones.map(( item) {
+      return Column(
+      children: <Widget>[ 
+      ListTile(
+        title: Text(item + '!'),
+        subtitle: Text('Subitulo'),
+        leading: Icon(Icons.account_balance_wallet),
+        trailing: Icon(Icons.keyboard_arrow_right),
+        onTap: () {},
+      ),
+      Divider()
+      ],
+    );
+    }).toList();
+
+     
+  }
+
 }
